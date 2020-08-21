0a8bc81d5569ed8dd83f3d6064d614278f46555c
arjunraj <arjunraj678@gmail.com>
2020-08-21 14:07:04 +0530

    first commit

---
+++ Bootstrap/bootstrap-4.5.0-dist/bootstrap-4.5.0-dist/js/floattext.js
@@ -0,0 +1,3 @@
+$('.is-floating-label input, .is-floating-label textarea').on('focus blur', function (e) {
+    $(this).parents('.is-floating-label').toggleClass('is-focused', (e.type === 'focus' || this.value.length > 0));
+}).trigger('blur');
